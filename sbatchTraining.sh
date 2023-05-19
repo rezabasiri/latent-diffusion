09673531a4690e405cd1aedfcb5da2bf1953e807
RezaB <90reza@gmail.com>
2023-05-19 13:19:33 -0400

    multinode

--- sbatchTraining.sh
+++ sbatchTraining.sh
@@ -12,6 +12,23 @@
 
 # DATA_DIR="/home/rbasiri/Dataset/GAN/train_foot/train/"
 
+
+
+echo myuser=`whoami`
+echo COUNT_NODE=$COUNT_NODE
+echo LD_LIBRARY_PATH = $LD_LIBRARY_PATH
+echo PATH = $PATH
+echo which mpicc `which mpicc`
+echo HOSTNAMES = $HOSTNAMES
+echo hostname = `hostname`
+echo MASTER_ADDR= $MASTER_ADDR
+echo MASTER_PORT= $MASTER_PORT
+
+H=`hostname`
+THEID=`echo -e $HOSTNAMES  | python3 -c "import sys;[sys.stdout.write(str(i)) for i,line in enumerate(next(sys.stdin).split(' ')) if line.strip() == '$H'.strip()]"`
+echo THEID=$THEID
+
+
 NUM_NODES=1
 GPUS=1
 # DEPTH=24
@@ -32,4 +49,4 @@ GPUS=1
 # OPENAI_LOGDIR=$OPENAI_LOGDIR \
 #     
 # torchrun --nproc_per_node=$GPUS --master_port=23456 --nnodes=$NUM_NODES /home/rbasiri/MyCode/Diffusion/latent-diffusion/Training.py
-accelerate launch /home/rbasiri/MyCode/Diffusion/latent-diffusion/Training.py
+accelerate launch  --num_processes $(( 2 * $COUNT_NODE )) --num_machines $COUNT_NODE --multi_gpu --mixed_precision fp16 --machine_rank $THEID --main_process_ip $MASTER_ADDR --main_process_port $MASTER_PORT /home/rbasiri/MyCode/Diffusion/latent-diffusion/Training.py
